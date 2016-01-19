2b357637ab2e1711bb17a3d30ec3f433b8d4ffe4
Tania <taniarascia@gmail.com>
2016-01-18 21:53:11 -0600

    Navigation

--- example/scroll.js
+++ example/scroll.js
@@ -20,18 +20,19 @@ $(document).ready(function () {
 			$(document).on("scroll", onScroll);
 		});
 	});
-});
 
-function onScroll(event) {
-	var scrollPos = $(document).scrollTop();
-	$('aside a').each(function () {
-		var currLink = $(this);
-		var refElement = $(currLink.attr("href"));
-		if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-			$('aside nav ul li a').removeClass("active");
-			currLink.addClass("active");
-		} else {
-			currLink.removeClass("active");
-		}
-	});
-}
+
+	function onScroll(event) {
+		var scrollPos = $(document).scrollTop();
+		$('aside a').each(function () {
+			var currLink = $(this);
+			var refElement = $(currLink.attr("href"));
+			if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+				$('aside nav ul li a').removeClass("active");
+				currLink.addClass("active");
+			} else {
+				currLink.removeClass("active");
+			}
+		});
+	}
+});
