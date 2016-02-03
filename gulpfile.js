b4333afee54b0410f10a2bba30b922e1a3fa8e9c
Tania <taniarascia@gmail.com>
2016-02-03 11:26:29 -0600

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cssnano())
+    .pipe(sourcemaps.write('./'))
+
+  .pipe(gulp.dest('./dist/css/'))
+});
+
+//Watch task
+gulp.task('default', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
