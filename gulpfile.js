d611414c2d5462bd83978821021da9529d4662c7
Valentin Deville <contact@valentin-deville.eu>
2016-11-12 21:50:15 +0100

    Add guilp-reanme for add .min before .css. Modify gulpfile.js and re-compile with good script (gulp file and not my file watcher IDE)

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,26 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
 
 gulp.task('workflow', function () {
-  gulp.src('./src/scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(cssnano({
-      zindex: false
-    }))
-    .pipe(sourcemaps.write('./'))
+    gulp.src('./src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano({
+            zindex: false
+        }))
+        .pipe(sourcemaps.write('./'))
 
-  .pipe(gulp.dest('./dist/css/'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+
+        .pipe(gulp.dest('./dist/css/'))
 });
 
 //Watch task
