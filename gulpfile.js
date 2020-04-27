4d3f3291ca5b7078148c22b13dce964d6426659c
Valentin Deville <contact@valentin-deville.eu>
2020-04-27 09:26:33 +0200

    Add gulp support

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const paths = {
+    srcCSS: './src/scss/**/*',
+    srcCSSMain: './src/scss/main.scss',
+    distCSS: './dist/css/'
+};
+
+
+function css() {
+    let plugins = [
+        autoprefixer(),
+        cssnano()
+    ];
+
+    return src(paths.srcCSSMain)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest(paths.distCSS))
+
+        .pipe(postcss(plugins))
+        .pipe(rename((path) => {
+            path.basename += ".min";
+        }))
+        .pipe(dest(paths.distCSS))
+}
+
+function looking() {
+    return watch(paths.srcCSS, css);
+}
+
+exports.css = css;
+exports.watch = looking;
+exports.default = css;
